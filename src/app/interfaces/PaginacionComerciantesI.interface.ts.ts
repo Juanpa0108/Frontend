60baf591c3ae879b296e8cc32f7a30d7cb0851a0
Juanpa0108 <109108926+Juanpa0108@users.noreply.github.com>
2025-02-11 19:05:23 -0500

    Delete src directory

--- src/app/interfaces/PaginacionComerciantesI.interface.ts.ts
+++
@@ -1,9 +0,0 @@
-import { ComercianteI } from "./Comerciante.interface";
-
-export interface PaginacionComerciantesI {
-    content: ComercianteI[];  
-    totalElements: number;
-    totalPages: number;
-    number: number;
-    size: number;
-  }
